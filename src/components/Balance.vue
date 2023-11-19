3468d2b32cc52002d285ca5bccebef51389feacf
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-11-19 23:34:17 +0100

    update: new features

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -33,18 +33,4 @@ const props = defineProps({
   },
 });
 
-// Reference to audio elements
-// const gainAudio = new Audio(GainAudio);
-// const lossAudio = new Audio(LossAudio);
-
-// // Watch for changes in the total and play audio accordingly
-// watch(() => props.total, (newValue, oldValue) => {
-//   if (newValue > oldValue) {
-//     // Play gain audio when the balance increases
-//     gainAudio.play();
-//   } else if (newValue < oldValue) {
-//     // Play loss audio when the balance decreases
-//     lossAudio.play();
-//   }
-// });
 </script>
